d6cd45986cbcebe4cdffe717826279588bd75763
Guenhy <eia.andersson@gmail.com>
2015-02-10 14:31:17 +0100

    First version of my blogtheme

---
+++ header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="My Site">
+        <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
+        <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <?php wp_enqueue_script("jquery"); ?>
+        <?php wp_head(); ?>
+    </head>
+    <div class="container">
+        <div class="my-nav-box">
+            <div class="container hidden-xs" id="my-container">
+                <img src="<?php echo get_bloginfo('template_url') ?>/img/header_image.png" id="my-img">
+                <img src="<?php echo get_bloginfo('template_url') ?>/img/header_logo.png" id="my-logo">
+            </div>
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand visible-xs" href="#">Code To My Life</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                           <?php wp_list_pages(array('title_li' => '')); ?>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
