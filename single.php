43f7d02a3c6cf46bd79c34752aff286406da82a8
guenhy <eia.andersson@gmail.com>
2015-04-22 18:20:34 +0200

    Updated the theme

--- single.php
+++ single.php
@@ -6,10 +6,10 @@
 
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 
-                <h1><?php the_title(); ?></h1>  
+                <h1><?php the_title(); ?></h1>
+                <p class="datetime">Published: <?php the_time( 'Y-n-j G:i' ); ?> </p>  
                 <?php the_content(); ?>
-                <?php //comment_form(); ?>
-                <?php //get_comments(); ?>
+                <?php comments_template(); ?> 
             <?php endwhile; else: ?>
                 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
             <?php endif; ?>
--- tag.php
+++ tag.php
@@ -1,20 +1,16 @@
 <?php get_header(); ?>
+
     <body>
     <div class="content">
     <div class="row">
         <div class="col-sm-8">
-
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <?php the_title( '<h2><a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark"> ',  '</a></h2>' ); ?>
-                <p class="datetime">Published: <?php the_time( 'Y-n-j G:i' ); ?> </p>
+                <?php the_time( 'Y-n-j G:i' ); ?> 
                 <?php the_excerpt(); ?> <a href="<?php the_permalink();?>">Read more</a>
             <?php endwhile; ?>
-
             <hr>
             <?php echo paginate_links( $args ); ?>
-
-            <?php wp_reset_postdata(); ?>
-
             <?php else: ?>
                 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
             <?php endif; ?>
